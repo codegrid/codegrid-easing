983683239232a2b98d2a2f8c9a6841733254ba63
Takeshi Takatsudo <takazudo@gmail.com>
2013-04-12 06:09:45 +0900

    updated demo

--- 02-animation/script.js
+++ 02-animation/script.js
@@ -4,9 +4,9 @@ var MILLISEC_PER_FRAME = 1000 / 60;
 // アニメーションさせる関数
 var animate = function($el) {
 
-  var currentVal = 0; // 初めの値
-  var valueInChange = 200; // 変化する値
-  var duration = 1000; // アニメーションさせる時間
+  var leftVal = 0; // left値
+  var valueInChange = 200; // 変化させるleft値
+  var duration = 1000; // トータル時間
   var elapsedTime = 0; // 過ぎた時間
 
   // 要素を動かす関数
@@ -14,20 +14,21 @@ var animate = function($el) {
 
     // 1000ミリ秒経ってたら最終値をセットして終わり
     if(elapsedTime >= 1000) {
-      currentVal = valueInChange;
-      $el.css('left', currentVal);
+      leftVal = valueInChange;
+      $el.css('left', leftVal);
       return;
     }
 
-    // 現在の値 = 変化させる値 * どのくらい時間が経ったかの割合
+    // 経過時間の割合 = 経過時間 / トータル時間
     var elapsedTimeRate = elapsedTime / duration;
-    currentVal = valueInChange * elapsedTimeRate;
+    // 現在の値 = 変化させるleft値 * 経過時間の割合
+    leftVal = valueInChange * elapsedTimeRate;
 
-    $el.css('left', currentVal);
+    // 要素のスタイルを更新
+    $el.css('left', leftVal);
 
-    // 1フレーム分の時間を加算
+    // 1フレーム分の時間を経過時間に加算
     elapsedTime += MILLISEC_PER_FRAME;
-
     // 1フレーム後に再び動かす
     setTimeout(tick, MILLISEC_PER_FRAME);
 
